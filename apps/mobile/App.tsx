7029a43e24f32d593d07ab6185ddd0504232d857
Lucas Queiroz <lsq@poli.br>
2026-05-21 21:09:58 -0300

    chore: restaurando telas do Kaiky e app.tsx

--- apps/mobile/App.tsx
+++ apps/mobile/App.tsx
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import RegistrarSinistro from "../mobile/src/presentation/screens/leaderscreens/Tela06-Sinistro"
+import SuppliesScreen from './src/presentation/screens/leaderscreens/SuppliesScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <RegistrarSinistro/>
+      <SuppliesScreen/>
     </View>
   );
 }
